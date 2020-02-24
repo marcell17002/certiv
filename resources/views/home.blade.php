95c97d27b59f6fb4addf00a7f4a2f3afdecd3d7c
marcell17002 <marcellantonius18@gmail.com>
2020-02-24 07:54:53 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!Doctype HTML>
+<html>
+    <head>
+        <title>
+            Welcome! - Certiv
+        </title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}" />
+    </head>
+    <body>
+        <div class="topnav">
+            <img class="logo" src={{asset('image/logo.png')}}>
+            <div class="topnav-right">
+                <a href="#search">Certification</a>
+                <a href="#about">Network</a>
+                <a href="#about">Article</a>
+                <a href="#about">Notifiation</a>
+                <a href="#about">Login</a>
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
