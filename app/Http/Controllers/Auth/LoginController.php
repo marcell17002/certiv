369e32564f29600c86ce7983f98bea7ed5952d79
adorabable <whatthefapmc@gmail.com>
2020-03-09 01:29:02 +0700

    able to login with username or email

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,39 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function username(){     //supaya login bisa pakek username atau email
+        $login = request()->input('identity');
+
+        $field = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username'; //untuk ngecek apakah itu username atau email
+        request()->merge([$field => $login]);  
+
+        return $field;    
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request)
+    {
+        $messages = [
+            'identity.required' => 'Email or username cannot be empty',
+            'email.exists' => 'Email or username already registered',
+            'username.exists' => 'Username is already registered',
+            'password.required' => 'Password cannot be empty',
+        ];
+
+        $request->validate([
+            'identity' => 'required|string',
+            'password' => 'required|string',
+            'email' => 'string|exists:users',
+            'username' => 'string|exists:users',
+        ], $messages);
     }
 }
